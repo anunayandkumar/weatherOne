a6f11c9a74942c95030c547c5a5982a91adf268d
Anunay Kumar <124424241+anunayandkumar@users.noreply.github.com>
2024-04-04 05:46:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import weatherBot
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}. To get weather update use command /weather <location> . Ex : /weather Kolkata')
 async def weather(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#    title = " ".join(context.args)
+    title = " ".join(context.args)
     weatherResult = weatherBot.getResults('Kolkata')
     await update.message.reply_text(weatherResult)
 
